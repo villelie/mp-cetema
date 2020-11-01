b5ae075daea0a9dd37072aa268478ba6021e4007
Ville Liekari <villelie@metropolia.fi>
2020-11-01 15:56:53 +0200

    test for failing api fetch

--- boat-navigation/test.js
+++ boat-navigation/test.js
@@ -6,4 +6,10 @@ describe('Test API call', () => {
         //if fetch url is incorrect it will return 404 == false assert
         chai.assert.equal(await fetch('https://meri.digitraffic.fi/api/v1/locations/latest').then(res => { return res.status }).catch(res => console.log(res)), 200);
     });
+
+    it('should return status 200, but this time with incorrect url', async () => {
+        //if fetch url is incorrect it will return 404 == false assert
+        //this time with incorrect url
+        chai.assert.equal(await fetch('https://meri.digitraffic.fi/api/v1/locations/latestttt').then(res => { return res.status }).catch(res => console.log(res)), 200);
+    });
 });

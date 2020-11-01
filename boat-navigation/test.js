2cfc2de4593f47ce3363f79bf6e3bad9395c5bba
Ville Liekari <villelie@metropolia.fi>
2020-11-01 15:54:19 +0200

    test for api fetch

--- boat-navigation/test.js
+++ boat-navigation/test.js
@@ -1,7 +1,9 @@
-const assert = require('assert');
+import chai from 'chai'
+import fetch from 'node-fetch';
 
-describe('Test', () => {
- it('should return ok', () => {
-        assert.strictEqual("ok", "ok");
+describe('Test API call', () => {
+    it('should return status 200', async () => {
+        //if fetch url is incorrect it will return 404 == false assert
+        chai.assert.equal(await fetch('https://meri.digitraffic.fi/api/v1/locations/latest').then(res => { return res.status }).catch(res => console.log(res)), 200);
     });
 });
